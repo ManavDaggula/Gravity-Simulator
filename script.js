e3906353bf0c237045c030e3800a2495169cf068
ManavDaggula <manavdaggula@gmail.com>
2023-01-14 20:01:51 +0530

    acceleration modified

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ var planets = [
 screen.addEventListener("mousemove",(event)=>{
     // planet.style.top = `${event.clientY-25}px`;
     // planet.style.left = `${event.clientX-5}px`;
-
     mouseStat.textContent = `${event.clientX-40}  ${event.clientY-78}`;
 })
 
@@ -55,51 +54,6 @@ screen.addEventListener("click",(event)=>{
 let inc=0;
 window.setInterval(() => {
     if(state==="play"){
-        // loop to calculate acceleration and speed components
-        for (const index in planets) {
-            if(planets.length<=1){break;}
-            // console.log(planet)
-            let COMx = 0, COMy = 0, COMmass = 0, newSpeedX = 0, newSpeedY = 0, accX,accY,accR,angle;
-            for(const i in planets){
-                if(i!==index){
-                    COMx += planets[i].mass*(planets[i].posX - planets[index].posX)
-                    COMy += planets[i].mass*(planets[i].posY - planets[index].posY)
-                    COMmass+=planets[i].mass
-                }
-            }
-            COMx=COMx/COMmass;
-            COMy=COMy/COMmass;
-            // console.log(`for ${index} ComX=${COMx/COMmass}, ComY=${COMy/COMmass},ComM=${COMmass}`)
-            // planets[index].elementRef.textContent = `x=${planets[index].posX},y=${planets[index].posY},i=${index} ComX=${COMx}, ComY=${COMy},ComM=${COMmass}`
-
-            // calculating acceleration components
-            accR = gravitationConst*COMmass/(Math.pow(COMx,2)+Math.pow(COMy,2))
-            accX = accR*COMx/(Math.pow(COMx,2)+Math.pow(COMy,2))
-            accY = accR*COMy/(Math.pow(COMx,2)+Math.pow(COMy,2))
-            // console.log(`for ${index} a=${accR} ax=${accX} ay=${accY}`)
-
-            //calculating new newSpeed components
-            planets[index].speedX = planets[index].speedX + accX*0.1
-            planets[index].speedY = planets[index].speedY + accY*0.1
-            // console.log(`for ${index} ax=${accX} ay=${accY} iSpeedX=${planets[index].speedX} iSpeedY=${planets[index].speedY}`)
-
-        }
-        
-        // loop to displace the planet
-        if(planets.length>1){
-            planets.forEach(planet => {
-                //calculating planets displacements
-                planet.posX = planet.posX + planet.speedX
-                planet.posY = planet.posY + planet.speedY
-    
-                //moving the planet to new position
-                planet.elementRef.style.left = `${planet.posX}px`
-                planet.elementRef.style.top = `${planet.posY}px`
-    
-                planet.elementRef.textContent = `speedX=${Math.round(planet.speedX*100)/100} speedY=${Math.round(planet.speedY*100)/100}`
-            });
-        }
-
         // loop to check planet collisions
         for (const index in planets){
             if(planets.length<=1){break;}
@@ -128,6 +82,58 @@ window.setInterval(() => {
                     planets.splice(i,1)
                 }
             }
+        }
+        
+        // loop to calculate acceleration and speed components
+        for (const index in planets) {
+            if(planets.length<=1){break;}
+            // console.log(planet)
+            // let COMx = 0, COMy = 0, COMmass = 0, newSpeedX = 0, newSpeedY = 0, accX=0,accY=0,accR=0,angle;
+            let rX,rY,rR,accR,accX=0,accY=0;
+            for(const i in planets){
+                if(i!==index){
+                    // COMx += planets[i].mass*(planets[i].posX - planets[index].posX)
+                    // COMy += planets[i].mass*(planets[i].posY - planets[index].posY)
+                    // COMmass+=planets[i].mass
+                    rX = planets[i].posX-planets[index].posX
+                    rY = planets[i].posY-planets[index].posY
+                    rR = Math.pow(rX,2)+Math.pow(rY,2)
+                    accR=gravitationConst*planets[i].mass/rR
+                    accX+=accR*rX/rR
+                    accY+=accR*rY/rR
+                }
+            }
+            // COMx=COMx/COMmass;
+            // COMy=COMy/COMmass;
+            // // console.log(`for ${index} ComX=${COMx/COMmass}, ComY=${COMy/COMmass},ComM=${COMmass}`)
+            // // planets[index].elementRef.textContent = `x=${planets[index].posX},y=${planets[index].posY},i=${index} ComX=${COMx}, ComY=${COMy},ComM=${COMmass}`
+
+            // // calculating acceleration components
+            // accR = gravitationConst*COMmass/(Math.pow(COMx,2)+Math.pow(COMy,2))
+            // accX = accR*COMx/(Math.pow(COMx,2)+Math.pow(COMy,2))
+            // accY = accR*COMy/(Math.pow(COMx,2)+Math.pow(COMy,2))
+            // // console.log(`for ${index} a=${accR} ax=${accX} ay=${accY}`)
+
+            //calculating new newSpeed components
+            planets[index].speedX = planets[index].speedX + accX*0.1
+            planets[index].speedY = planets[index].speedY + accY*0.1
+            // console.log(`for ${index} ax=${accX} ay=${accY} iSpeedX=${planets[index].speedX} iSpeedY=${planets[index].speedY}`)
+
+        }
+        
+        // loop to displace the planet
+        if(planets.length>1){
+            planets.forEach(planet => {
+                //calculating planets displacements
+                planet.posX = planet.posX + planet.speedX
+                planet.posY = planet.posY + planet.speedY
+    
+                //moving the planet to new position
+                planet.elementRef.style.left = `${planet.posX}px`
+                planet.elementRef.style.top = `${planet.posY}px`
+    
+                planet.elementRef.textContent = `speedX=${Math.round(planet.speedX*100)/100} speedY=${Math.round(planet.speedY*100)/100}`
+            });
         }
 
         console.log(planets)
