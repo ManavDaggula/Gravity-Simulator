f42553a67614c1641cae7dde3c0d0d67c8f1492a
ManavDaggula <manavdaggula@gmail.com>
2023-01-22 14:07:44 +0530

    at least working in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+require("./script")
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="menu">
+        <li>New Planet</li>
+        <li>Clear</li>
+    </ul>
+    <div className="mouse-stat">move your curser inside screen</div>
+    <div className="screen">
+      {/* planets inserted here */}
+    </div>
+    <div className="prompt"></div>
     </div>
   );
 }
